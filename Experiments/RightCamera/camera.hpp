c4b46ae5d2ec9a6bd18d69ec2f9d93509842620b
Yuan Yang <136157134+yuanyang0563@users.noreply.github.com>
2025-04-20 00:32:03 +0800

    Add files via upload

--- Experiments/RightCamera/camera.hpp
+++ Experiments/RightCamera/camera.hpp
@@ -20,12 +20,13 @@ class camera {
   	std_msgs::Float64MultiArray msg;
   	vpDisplay *display;
   	vpCameraParameters cam;
-  	vpImage<vpRGBa> image_color;
-  	vpImage<unsigned char> image_grey;
-  	vector<vector<vpImagePoint>> tagsCorners;
-  	vpDetectorAprilTag detector;
-  	vpRealSense2 g;
-  	rs2::config config;
+	vpImage<vpRGBa> image_color;
+	vpImage<unsigned char> image_grey;
+	vector<vector<vpImagePoint>> current_corners, initial_corners;
+	vpDetectorAprilTag detector;
+	vpRealSense2 g;
+	rs2::config config;
+	bool getImage;
   	
   public:
   	camera (string name) {
@@ -36,11 +37,12 @@ class camera {
   		display = new vpDisplayX(image_color);
   		vpDisplay::setTitle(image_color,name+"_image");
   		config.disable_stream(RS2_STREAM_DEPTH);
-  		config.disable_stream(RS2_STREAM_INFRARED);
-  		config.enable_stream(RS2_STREAM_COLOR,1920,1080,RS2_FORMAT_RGBA8,30);
-  		g.open(config);
-  		g.acquire(image_color);
-  		cam = g.getCameraParameters(RS2_STREAM_COLOR,vpCameraParameters::perspectiveProjWithDistortion);
+		config.disable_stream(RS2_STREAM_INFRARED);
+		config.enable_stream(RS2_STREAM_COLOR,1920,1080,RS2_FORMAT_RGBA8,30);
+		g.open(config);
+		g.acquire(image_color);
+		cam = g.getCameraParameters(RS2_STREAM_COLOR,vpCameraParameters::perspectiveProjWithDistortion);
+		getImage = false;
   	}
   	
   	~camera() {
@@ -48,23 +50,30 @@ class camera {
   	}
   	
   	void getFeature () {
-  		while (ros::ok()) {
-  			g.acquire(image_color);
-  			vpImageConvert::convert(image_color,image_grey);
-  			bool status = detector.detect(image_grey);
-  			if (status) {
-  				tagsCorners = detector.getTagsCorners();
-  				for (int i=0; i<4; ++i)
-  					vpPixelMeterConversion::convertPoint(cam,tagsCorners[0][i],msg.data[2*i+0],msg.data[2*i+1]);
-  				pub.publish(msg);
-  			}
-  			vpDisplay::display(image_color);
-  			vpDisplay::flush(image_color);
-  			if (vpDisplay::getClick(image_color,false))
-  				break;
-  			ros::spinOnce();
-  			loop_rate->sleep();
-  		}
+		while (ros::ok()) {
+			g.acquire(image_color);
+			vpDisplay::display(image_color);
+			vpImageConvert::convert(image_color,image_grey);
+			bool status = detector.detect(image_grey);
+			if (status) {
+				current_corners = detector.getTagsCorners();
+				if (!getImage) {
+					initial_corners = current_corners;
+					getImage = !getImage;
+				}
+				for (int i=0; i<4; ++i) {
+					vpDisplay::displayCross(image_color,initial_corners[0][i],15,vpColor::blue,3);
+					vpDisplay::displayCross(image_color,current_corners[0][i],15,vpColor::red,3);
+					vpPixelMeterConversion::convertPoint(cam,current_corners[0][i],msg.data[2*i+0],msg.data[2*i+1]);
+				}
+				pub.publish(msg);
+			}
+			vpDisplay::flush(image_color);
+			if (vpDisplay::getClick(image_color,false))
+				break;
+			ros::spinOnce();
+			loop_rate->sleep();
+		}
   	}
 
 };
