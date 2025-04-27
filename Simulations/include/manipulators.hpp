c842ec018fa622c5e3cd03b542d0812e4a32aa92
Yuan Yang <136157134+yuanyang0563@users.noreply.github.com>
2025-04-27 21:06:03 +0800

    Update manipulators.hpp

--- Simulations/include/manipulators.hpp
+++ Simulations/include/manipulators.hpp
@@ -13,6 +13,9 @@ class manipulators {
 	MatrixXd A_d, A_s, A_v;
 	VectorXd b_d, b_s, b_v;
 	
+	Vector3d x0_lr, x0_rl, x_lr, x_rl;
+	Matrix3d R0_lr, R0_rl, R_lr, R_rl;
+	
 	manipulators (string arm_left, string arm_right) : left(arm_left), right(arm_right) {
 		A_d = MatrixXd::Zero(12*N,12*N);
 		b_d = VectorXd::Zero(12*N);
@@ -37,54 +40,86 @@ class manipulators {
 	void set_tar_pars () {
 		left.set_tar_pars();
 		right.set_tar_pars();
-		A_d.block(0,0,3*N,3*N) = left.Au_d;
-		A_d.block(3*N,3*N,3*N,3*N) = right.Au_d;
-		A_d.block(6*N,6*N,3*N,3*N) = left.Ao_d;
-		A_d.block(9*N,9*N,3*N,3*N) = right.Ao_d;
+		A_d.block(0*N,0*N,3*N,3*N) = left.A_d.block(0*N,0*N,3*N,3*N);
+		A_d.block(3*N,3*N,3*N,3*N) = right.A_d.block(0*N,0*N,3*N,3*N);
+		A_d.block(6*N,6*N,3*N,3*N) = left.A_d.block(3*N,3*N,3*N,3*N);
+		A_d.block(9*N,9*N,3*N,3*N) = right.A_d.block(3*N,3*N,3*N,3*N);
 	}
 	
 	void update_tar_pars () {
 		left.update_tar_pars();
 		right.update_tar_pars();
-		b_d << left.bu_d, right.bu_d, left.bo_d, right.bo_d;
+		b_d << left.b_d.head(3*N), right.b_d.head(3*N), left.b_d.tail(3*N), right.b_d.tail(3*N);
 	}
-	
+
 	void set_syn_pars () {
-		A_s.block(0,0,3*N,3*N) = rho_u*kroneckerProduct(Snn,Matrix3d::Identity());
-		A_s.block(0,3*N,3*N,3*N) = -rho_u*kroneckerProduct(Snn,left.Rd*right.Rd.transpose());
-		A_s.block(3*N,0,3*N,3*N) = -rho_u*kroneckerProduct(Snn,right.Rd*left.Rd.transpose());
-		A_s.block(3*N,3*N,3*N,3*N) = rho_u*kroneckerProduct(Snn,Matrix3d::Identity());
+		left.get_pose_jacobian();
+		right.get_pose_jacobian();
+		left.x0 = left.x;
+		left.R0 = left.R;
+		right.x0 = right.x;
+		right.R0 = right.R;
+		x0_lr = left.R0.transpose()*(right.x0-left.x0);
+		x0_rl = right.R0.transpose()*(left.x0-right.x0);
+		R0_lr = left.R0.transpose()*right.R0;
+		R0_rl = right.R0.transpose()*left.R0;
+		A_s.block(0*N,0*N,3*N,3*N)  =  rho_u*kroneckerProduct(Snn,4.0*Matrix3d::Identity());
+		A_s.block(0*N,3*N,3*N,3*N)  = -rho_u*kroneckerProduct(Snn,4.0*Matrix3d::Identity());
+		A_s.block(3*N,0*N,3*N,3*N)  = -rho_u*kroneckerProduct(Snn,4.0*Matrix3d::Identity());
+		A_s.block(3*N,3*N,3*N,3*N)  =  rho_u*kroneckerProduct(Snn,4.0*Matrix3d::Identity());
+		A_s.block(6*N,6*N,3*N,3*N)  = -rho_u*kroneckerProduct(Snn,skewMat(x0_lr)*skewMat(x0_lr));
+		A_s.block(9*N,9*N,3*N,3*N)  = -rho_u*kroneckerProduct(Snn,skewMat(x0_rl)*skewMat(x0_rl));
+		A_s.block(0*N,0*N,3*N,3*N) +=  beta_u*MatrixXd::Identity(3*N,3*N);
+		A_s.block(3*N,3*N,3*N,3*N) +=  beta_u*MatrixXd::Identity(3*N,3*N);
+		A_s.block(6*N,6*N,3*N,3*N) +=  beta_o*MatrixXd::Identity(3*N,3*N);
+		A_s.block(9*N,9*N,3*N,3*N) +=  beta_o*MatrixXd::Identity(3*N,3*N);
 	}
 
 	void update_syn_pars () {
-		b_s.segment(0,3*N) = rho_u*Sn*(left.x-left.xd-left.Rd*right.Rd.transpose()*(right.x-right.xd));
-		b_s.segment(3*N,3*N) = rho_u*Sn*(right.x-right.xd-right.Rd*left.Rd.transpose()*(left.x-left.xd));
-		b_s.segment(6*N,3*N) = rho_o*Sn*skewVec(right.R.transpose()*right.Rd*left.Rd.transpose()*left.R);
-		b_s.segment(9*N,3*N) = rho_o*Sn*skewVec(left.R.transpose()*left.Rd*right.Rd.transpose()*right.R);
-		A_s.block(6*N,9*N,3*N,3*N) = 0.5*rho_o*kroneckerProduct(Snn,right.R.transpose()*right.Rd*left.Rd.transpose()*left.R-(right.R.transpose()*right.Rd*left.Rd.transpose()*left.R).trace()*Matrix3d::Identity());
-		A_s.block(9*N,6*N,3*N,3*N) = 0.5*rho_o*kroneckerProduct(Snn,left.R.transpose()*left.Rd*right.Rd.transpose()*right.R-(left.R.transpose()*left.Rd*right.Rd.transpose()*right.R).trace()*Matrix3d::Identity());
+		x_lr = left.R.transpose()*(right.x-left.x);
+		x_rl = right.R.transpose()*(left.x-right.x);
+		R_lr = left.R.transpose()*right.R;
+		R_rl = right.R.transpose()*left.R;
+		A_s.block(0*N,6*N,3*N,3*N)  = -rho_u*kroneckerProduct(Snn,2.0*left.R*skewMat(x0_lr));
+		A_s.block(0*N,9*N,3*N,3*N)  =  rho_u*kroneckerProduct(Snn,2.0*right.R*skewMat(x0_rl));
+		A_s.block(3*N,6*N,3*N,3*N)  =  rho_u*kroneckerProduct(Snn,2.0*left.R*skewMat(x0_lr));
+		A_s.block(3*N,9*N,3*N,3*N)  = -rho_u*kroneckerProduct(Snn,2.0*right.R*skewMat(x0_rl));
+		A_s.block(6*N,0*N,3*N,3*N)  =  rho_u*kroneckerProduct(Snn,2.0*skewMat(x0_lr)*left.R.transpose());
+		A_s.block(6*N,3*N,3*N,3*N)  = -rho_u*kroneckerProduct(Snn,2.0*skewMat(x0_lr)*left.R.transpose());
+		A_s.block(9*N,0*N,3*N,3*N)  = -rho_u*kroneckerProduct(Snn,2.0*skewMat(x0_rl)*right.R.transpose());
+		A_s.block(9*N,3*N,3*N,3*N)  =  rho_u*kroneckerProduct(Snn,2.0*skewMat(x0_rl)*right.R.transpose());
+		A_s.block(6*N,9*N,3*N,3*N)  =  rho_u*kroneckerProduct(Snn,skewMat(x0_lr)*R_lr*skewMat(x0_rl));
+		A_s.block(6*N,9*N,3*N,3*N) +=  rho_o*kroneckerProduct(Snn,R_lr*R0_lr.transpose()*R_lr-R_lr*(R0_lr.transpose()*R_lr).trace());
+		A_s.block(9*N,6*N,3*N,3*N)  =  rho_u*kroneckerProduct(Snn,skewMat(x0_rl)*R_rl*skewMat(x0_lr));
+		A_s.block(9*N,6*N,3*N,3*N) +=  rho_o*kroneckerProduct(Snn,R_rl*R0_rl.transpose()*R_rl-R_rl*(R0_rl.transpose()*R_rl).trace());
+		b_s.segment(0*N,3*N)  = rho_u*Sn*(2.0*left.R*(x0_lr-x_lr)-2.0*right.R*(x0_rl-x_rl));
+		b_s.segment(3*N,3*N)  = rho_u*Sn*(2.0*right.R*(x0_rl-x_rl)-2.0*left.R*(x0_lr-x_lr));
+		b_s.segment(6*N,3*N)  = rho_u*Sn*(skewMat(x0_lr)*R_lr*(2.0*x_rl-x0_rl));
+		b_s.segment(6*N,3*N) += rho_o*Sn*(2.0*skewVec(R0_lr*R_lr.transpose()));
+		b_s.segment(9*N,3*N)  = rho_u*Sn*(skewMat(x0_rl)*R_rl*(2.0*x_lr-x0_lr));
+		b_s.segment(9*N,3*N) += rho_o*Sn*(2.0*skewVec(R0_rl*R_rl.transpose()));
+	}
+	
+	void set_vis_pars () {
+		left.set_vis_pars();
+		right.set_vis_pars();
 	}
 	
 	void update_vis_pars () {
-		A_v.setZero();
-		b_v.setZero();
-		MatrixXd A, B, C;
-		for (int i=0; i<4; ++i) {
-			A = left.R*left.Rec*left.L[i].transpose();
-			B = skewMat(left.Reo*left.p[i]+left.xeo)*left.Rec*left.L[i].transpose();
-			C = skewMat(right.Reo*left.p[i]+right.xeo)*right.R.transpose()*left.R*left.Rec*left.L[i].transpose();
-			MatrixXd Lt_left(12*N,2);
-			Lt_left << -Sn*A, Sn*A, -Sn*B, Sn*C;
-			A_v += gamma_v*Lt_left*Lt_left.transpose();
-			b_v += gamma_v*Lt_left*(left.zeta[i]-left.zeta_d[i]);
-			A = right.R*right.Rec*right.L[i].transpose();
-			B = skewMat(left.Reo*right.p[i]+left.xeo)*left.R.transpose()*right.R*right.Rec*right.L[i].transpose();
-			C = skewMat(right.Reo*right.p[i]+right.xeo)*right.Rec*right.L[i].transpose();
-			MatrixXd Lt_right(12*N,2);
-			Lt_right << Sn*A, -Sn*A, Sn*B, -Sn*C;
-			A_v += gamma_v*Lt_right*Lt_right.transpose();
-			b_v += gamma_v*Lt_right*(right.zeta[i]-right.zeta_d[i]);
-		}
+		left.update_vis_pars();
+		right.update_vis_pars();
+		A_v.block(0*N,0*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,left.Lu.transpose()*left.Lu);
+		A_v.block(0*N,6*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,left.Lu.transpose()*left.Lo);
+		A_v.block(3*N,3*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,right.Lu.transpose()*right.Lu);
+		A_v.block(3*N,9*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,right.Lu.transpose()*right.Lo);
+		A_v.block(6*N,0*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,left.Lo.transpose()*left.Lu);
+		A_v.block(6*N,6*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,left.Lo.transpose()*left.Lo);
+		A_v.block(9*N,3*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,right.Lo.transpose()*right.Lu);
+		A_v.block(9*N,9*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,right.Lo.transpose()*right.Lo);
+		b_v.segment(0*N,3*N) = gamma_v*Sn*left.Lu.transpose()*(left.zeta-left.zeta_d);
+		b_v.segment(3*N,3*N) = gamma_v*Sn*right.Lu.transpose()*(right.zeta-right.zeta_d);
+		b_v.segment(6*N,3*N) = gamma_v*Sn*left.Lo.transpose()*(left.zeta-left.zeta_d);
+		b_v.segment(9*N,3*N) = gamma_v*Sn*right.Lo.transpose()*(right.zeta-right.zeta_d);
 	}
 	
 	inline double cost () {
@@ -92,8 +127,3 @@ class manipulators {
 	}
 
 };
-
-
-
-
-
