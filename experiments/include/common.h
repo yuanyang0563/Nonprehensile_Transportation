a681fc445cb09d1ae5e51aa71d1c554ade3dcb23
Yuan Yang <136157134+yuanyang0563@users.noreply.github.com>
2025-05-03 03:02:30 +0800

    Add files via upload

---
+++ experiments/include/common.h
@@ -0,0 +1,114 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/Pose.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Geometry>
+#include <visp3/core/vpImage.h>
+#include <visp3/core/vpConfig.h>
+#include <visp3/core/vpDisplay.h>
+#include <visp3/gui/vpDisplayX.h>
+#include <visp3/core/vpCameraParameters.h>
+#include <visp3/core/vpPixelMeterConversion.h>
+#include <visp3/detection/vpDetectorAprilTag.h>
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <ctime>
+
+using namespace std;
+using namespace Eigen;
+
+
+const int N = 5;
+
+MatrixXf Gu(3,12);
+MatrixXf Go(3,12);
+MatrixXf Sn(3*N,3);
+MatrixXf Snn(N,N);
+MatrixXf Gamma(N,N);
+Vector3f g(0.0,0.0,9.81);
+
+float m = 0.5;
+float I1 = 0.1;
+float I2 = 0.1;
+float I3 = 0.1;
+float mu = 0.1;
+DiagonalMatrix<float,3> I;
+
+Vector3f r1( 0.075,-0.075,-0.075);
+Vector3f r2( 0.075, 0.075,-0.075);
+Vector3f r3(-0.075, 0.075,-0.075);
+Vector3f r4(-0.075,-0.075,-0.075);
+MatrixXf r(4,3);
+
+float dt = 0.01;
+float vt = 1.0;
+float vr = 1.0;
+float fc = 1e5;
+float epsilon = 0.01;
+float kappa_u = 1.0;
+float alpha_u = 300.0;
+float kappa_o = 0.2;
+float alpha_o = 150.0;
+float rho_u = 2.0;
+float beta_u = 50.0;
+float rho_o = 2.0;
+float beta_o = 50.0;
+float gamma_v = 0.05;
+float kappa_f = 0.0;
+float rho_f = 0.0;
+float kappa_pid = 1.0;
+float rho_pid = 5.0;
+float gamma_pid = 0.5;
+
+Matrix3f skewMat (const Vector3f& w);
+Vector3f skewVec (const Matrix3f& M);
+MatrixXf kroneckerProduct (const MatrixXf& A, const MatrixXf& B);
+
+void init () {
+  I.diagonal() << I1, I2, I3;
+  r << r1.transpose(), r2.transpose(), r3.transpose(), r4.transpose();
+  Gamma = MatrixXf::Identity(N,N);
+  for (size_t n=1; n<N; ++n)
+	Gamma(n,n-1) = -1.0;
+  Sn.setZero();
+  Snn.setZero();
+  for (size_t n=0; n<N; ++n) {
+	MatrixXf sn = MatrixXf::Zero(N,1);
+	for (size_t i=0; i<=n; ++i)
+		sn(i) = 1.0;
+	Sn += kroneckerProduct(sn,Matrix3f::Identity());
+	Snn += sn*sn.transpose();
+  }
+  for (size_t i=0; i<4; ++i) {
+	Gu.block(0,3*i,3,3) = Matrix3f::Identity();
+	Go.block(0,3*i,3,3) = skewMat(r.row(i));
+  }
+}
+
+
+Matrix3f skewMat (const Vector3f& w) {
+    Matrix3f W;
+    W << 0.0, -w(2), w(1), w(2), 0.0, -w(0), -w(1), w(0), 0.0;
+    return W;
+}
+
+Vector3f skewVec (const Matrix3f& M) {
+    Vector3f m;
+    m << M(2,1), M(0,2), M(1,0);
+    return m;
+}
+
+MatrixXf kroneckerProduct (const MatrixXf& A, const MatrixXf& B) {
+    MatrixXf result(A.rows()*B.rows(), A.cols()*B.cols());
+    for (size_t i = 0; i < A.rows(); ++i) {
+        for (size_t j=0; j<A.cols(); ++j)
+            result.block(i*B.rows(), j*B.cols(), B.rows(), B.cols()) = A(i,j)*B;
+    }
+    return result;
+}
---
+++ experiments/src/mpc_dual.cpp
@@ -0,0 +1,136 @@
+#include <gurobi_c++.h>
+#include "mpc_dual.hpp"
+
+int main(int argc, char *argv[])
+{
+	init();
+	// set ROS
+	ros::init(argc,argv,"mpc_dual");
+	// set control mode
+	float mode;
+	if (argc<2) {
+		cout << "Please set the control mode:" << endl;
+		cout << "0 for visual servoing." << endl;
+		cout << "1 for target reaching." << endl;
+		cout << "2 for combined control." << endl;
+		return 1;
+	} else {
+		mode = stod(argv[1]);
+		if (mode!=0.0 && mode!=1.0 && mode!=2.0) {
+			ROS_ERROR("Wrong mode.");
+			return 1;
+		}
+	}
+	// create the arms object
+	manipulator_dual arms("arm_l","arm_r",mode);
+	// set the DH parameters of arms
+	arms.left.a << 0.0, -0.24365, -0.21325, 0.0, 0.0, 0.0;
+	arms.left.alpha << M_PI/2.0, 0.0, 0.0, M_PI/2.0, -M_PI/2.0, 0.0;
+	arms.left.d << 0.15190, 0.0, 0.0, 0.11235, 0.08535, 0.0819;
+	arms.right.a << 0.0, -0.24355, -0.21320, 0.0, 0.0, 0.0;
+	arms.right.alpha << M_PI/2.0, 0.0, 0.0, M_PI/2.0, -M_PI/2.0, 0.0;
+	arms.right.d << 0.15185, 0.0, 0.0, 0.13105, 0.08535, 0.0921;
+	// set the object poses relative to the end-effector frames of arms
+	arms.left.xeo << 0.0, -0.20, -0.05;
+	arms.left.Reo << 1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, -1.0;
+	arms.right.xeo << 0.0, 0.20, -0.05;
+	arms.right.Reo << 1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, -1.0;
+	// set the poses of camera frames relative to tne end-effector frames of arms
+	arms.left.xec << 0.035, 0.050, -0.075;
+	arms.left.Rec << 0.0, -1.0, 0.0, 0.0, 0.0, -1.0, 1.0, 0.0, 0.0;
+	arms.right.xec << -0.035, -0.050, -0.075;
+	arms.right.Rec << 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0;
+	// set the poses of arm bases
+	arms.left.xb << 0.0, -0.44, 0.0;
+	arms.left.Rb << cos(3.0*M_PI/4.0), -sin(3.0*M_PI/4.0), 0.0, sin(3.0*M_PI/4.0), cos(3.0*M_PI/4.0), 0.0, 0.0, 0.0, 1.0;
+	arms.right.xb << 0.0, 0.44, 0.0;
+	arms.right.Rb << cos(M_PI/4.0), -sin(M_PI/4.0), 0.0, sin(M_PI/4.0), cos(M_PI/4.0), 0.0, 0.0, 0.0, 1.0;
+	// set the desired poses of arms
+	arms.left.xd << 0.4008, -0.1000, 0.15;
+	arms.left.Rd << cos(M_PI/4.0), sin(M_PI/4.0), 0.0, sin(M_PI/4.0), -cos(M_PI/4.0), 0.0, 0.0, 0.0, -1.0;
+	arms.right.xd << 0.1392, 0.1616, 0.15;
+	arms.right.Rd << cos(M_PI/4.0), sin(M_PI/4.0), 0.0, sin(M_PI/4.0), -cos(M_PI/4.0), 0.0, 0.0, 0.0, -1.0;
+	// set parameters for Gurobi to solve the problem
+	arms.set_opt_pars();
+	// call Gurobi to perform optimal control
+	GRBEnv env = GRBEnv();
+	env.set(GRB_IntParam_OutputFlag, 0);
+	while (ros::ok()) {
+		// record the starting time of each round
+		auto t_start = chrono::high_resolution_clock::now();
+		// get the current robot poses and Jacobians
+		arms.get_pose_jacobian();
+		// update parameters for Gurobi to the solve the problem
+		arms.update_opt_pars();
+		if (arms.getJoints && arms.getFeatures) {
+			if (!arms.isInitialized) {
+				this_thread::sleep_for(chrono::seconds(5));
+				arms.isInitialized = true;
+			}
+			try {
+				// create a gurobi model and add optimization variables uof with lower and upper bounds
+				GRBModel model = GRBModel(env);
+				GRBVar *uof = model.addVars(arms.uof_lb,arms.uof_ub,NULL,NULL,NULL,24*N);
+				// set the objective function
+				GRBQuadExpr obj = 0.0;
+				for (size_t i=0; i<24*N; ++i) {
+					for (size_t j=0; j<24*N; ++j)
+						obj += dt*uof[i]*arms.A_obj(i,j)*uof[j];
+					obj += 2.0*arms.b_obj(i)*uof[i];
+				}
+				model.setObjective(obj);
+				// add motion constraints on the transported object
+				for (size_t i=0; i<6*N; ++i) {
+					GRBLinExpr cst = 0.0;
+					for (size_t j=0; j<3*N; ++j)
+						cst += arms.left.Hu(i,j-0*N)*uof[j];
+					for (size_t j=3*N; j<6*N; ++j)
+						cst -= arms.left.Ho(i,j-3*N)*uof[j];
+					for (size_t j=6*N; j<9*N; ++j)
+						cst += arms.right.Hu(i,j-6*N)*uof[j];
+					for (size_t j=9*N; j<12*N; ++j)
+						cst -= arms.right.Ho(i,j-9*N)*uof[j];
+					for (size_t j=12*N; j<24*N; ++j) {
+						cst -= dt*arms.left.Hf(i,j-12*N)*uof[j];
+						cst -= dt*arms.right.Hf(i,j-12*N)*uof[j];
+					}
+					model.addConstr(cst==arms.left.h(i)+arms.right.h(i));
+				}
+				// add constraints on the contact forces between the object and the tray
+				for (size_t n=0; n<N; ++n) {
+					for (size_t i=0; i<4; ++i) {
+						size_t ind = 12*N+12*n+3*i;
+						GRBQuadExpr cstc = uof[ind+0]*uof[ind+0]+uof[ind+1]*uof[ind+1]-mu*mu*uof[ind+2]*uof[ind+2];
+						model.addQConstr(cstc<=0.0);
+					}
+				}
+				// solve and set the Cartesian velocity control inputs
+				model.optimize();
+				arms.left.upsilon << uof[0].get(GRB_DoubleAttr_X), uof[1].get(GRB_DoubleAttr_X), uof[2].get(GRB_DoubleAttr_X);
+				arms.left.omega << uof[3*N].get(GRB_DoubleAttr_X), uof[3*N+1].get(GRB_DoubleAttr_X), uof[3*N+2].get(GRB_DoubleAttr_X);
+				arms.right.upsilon << uof[6*N].get(GRB_DoubleAttr_X), uof[6*N+1].get(GRB_DoubleAttr_X), uof[6*N+2].get(GRB_DoubleAttr_X);
+				arms.right.omega << uof[9*N].get(GRB_DoubleAttr_X), uof[9*N+1].get(GRB_DoubleAttr_X), uof[9*N+2].get(GRB_DoubleAttr_X);
+				for (size_t i=0; i<12; ++i) {
+					arms.left.f(i) = uof[12*N+i].get(GRB_DoubleAttr_X);
+					arms.right.f(i) = uof[12*N+i].get(GRB_DoubleAttr_X);
+				}
+			} catch(GRBException e) {
+				cout << "No solution. Reduce N or increase alpha." << endl;
+			} catch(...) {
+				cout << "Exception during optimization." << endl;
+			}
+		}
+		arms.move_one_step();
+		// count the time spent and store data
+		auto t_stop = chrono::high_resolution_clock::now();
+    		auto t_duration = chrono::duration<float>(t_stop-t_start);
+		arms.store_data(t_duration.count());
+    		// fix the time duration of each round
+    		if (t_duration.count()<dt)
+    			this_thread::sleep_for(chrono::duration<float>(dt-t_duration.count()));
+	}
+	arms.stop_moving();
+	
+	return 0;
+}
+
