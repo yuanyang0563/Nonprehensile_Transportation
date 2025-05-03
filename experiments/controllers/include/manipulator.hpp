81978b52485cde7d53d8420416329f127f330f8a
Yuan Yang <136157134+yuanyang0563@users.noreply.github.com>
2025-05-04 02:11:13 +0800

    Add files via upload

---
+++ experiments/controllers/include/manipulator.hpp
@@ -0,0 +1,201 @@
+#include "common.h"
+
+class manipulator {
+
+  protected:
+  	ros::NodeHandle nh;
+	ros::Publisher pub_jv;
+	ros::Subscriber sub_js;
+	ros::Subscriber sub_if;
+	std_msgs::Float64MultiArray jv_msg;
+	vector<float*> data;
+	stringstream file_name;
+	bool file_init;
+
+  public:
+  	VectorXf a, alpha, d;
+	VectorXf q, dq, f;
+	VectorXf b_d, b_v, eta, lambda, h;
+	MatrixXf A_d, A_v, J, Hu, Ho, Hf, Tu, To, L, Lu, Lo, Lm;
+	Vector3f upsilon, omega;
+	Vector3f xb, xeo, xec, xd, x, x0;
+	Matrix3f Rb, Reo, Rec, Rd, R, R0;
+	bool getJoints, getFeatures;
+	VectorXf zeta, zeta_d;
+	MatrixXf zeta_h;
+	
+	manipulator (string name) {
+		a = VectorXf(6);
+		alpha = VectorXf(6);
+		d = VectorXf(6);
+		q = VectorXf(6);
+		dq = VectorXf(6);
+		f = VectorXf::Zero(12);
+		upsilon = Vector3f::Zero();
+		omega = Vector3f::Zero();
+		J = MatrixXf(6,6);
+		Hu = MatrixXf::Zero(6*N,3*N);
+		Ho = MatrixXf::Zero(6*N,3*N);
+		Hf = MatrixXf::Zero(6*N,12*N);
+		eta = VectorXf::Zero(3*N);
+		lambda = VectorXf::Zero(3*N);
+		h = VectorXf::Zero(6*N);
+		A_d = MatrixXf::Zero(6*N,6*N);
+		b_d = VectorXf::Zero(6*N);
+		A_v = MatrixXf::Zero(6*N,6*N);
+		b_v = VectorXf::Zero(6*N);
+		pub_jv = nh.advertise<std_msgs::Float64MultiArray>(name+"/joint_group_vel_controller/command",1);
+		jv_msg.data.resize(6);
+		sub_js = nh.subscribe(name+"/joint_states",1,&manipulator::joint_callback,this);
+		getJoints = false;
+		sub_if = nh.subscribe(name+"/image_features",1,&manipulator::feature_callback,this);
+		getFeatures = false;
+		Tu = MatrixXf::Zero(6,3);
+		To = MatrixXf::Zero(6,3);
+		L = MatrixXf::Zero(8,6);
+		Lu = MatrixXf::Zero(8,3);
+		Lo = MatrixXf::Zero(8,3);
+		Lm = MatrixXf::Zero(8,6);
+		zeta = VectorXf(8);
+		zeta_d = VectorXf(8);
+		zeta_h = MatrixXf(8,8);
+		data.resize(32);
+		for (size_t i=0; i<6; ++i)
+			data[i] = &q(i);
+		for (size_t i=6; i<12; ++i)
+			data[i] = &dq(i-6);
+		for (size_t i=12; i<20; ++i)
+			data[i] = &zeta(i-12);
+		for (size_t i=20; i<32; ++i)
+			data[i] = &f(i-20);
+		file_name << "../data/" << ros::this_node::getName() << "_" << name << "_" << time(0) << ".txt";
+		file_init = false;
+	}
+	
+	virtual void get_pose_jacobian () {
+		Matrix4f A;
+		MatrixXf T = Matrix4f::Identity();
+		Matrix<float, 3, 7> o, z;
+		o.col(0) << 0.0, 0.0, 0.0;
+		z.col(0) << 0.0, 0.0, 1.0;
+		for (size_t i=0; i<6; ++i) {
+			A << cos(q(i)), -sin(q(i))*cos(alpha(i)),  sin(q(i))*sin(alpha(i)), a(i)*cos(q(i)),
+	     	     	     sin(q(i)),  cos(q(i))*cos(alpha(i)), -cos(q(i))*sin(alpha(i)), a(i)*sin(q(i)),
+	     	             0.0,        sin(alpha(i)),            cos(alpha(i)),           d(i),
+	     	             0.0,        0.0,                      0.0,                     1.0;
+	     		T *= A;
+			o.col(i+1) << T.block(0,3,3,1);
+			z.col(i+1) << T.block(0,2,3,1);
+		}
+		x = Rb*T.block(0,3,3,1)+xb;
+		R = Rb*T.block(0,0,3,3);
+		for (size_t i=0; i<6; ++i)
+			J.col(i)<< Rb*z.col(i).cross(o.col(6)-o.col(i)), R.transpose()*Rb*z.col(i);
+	}
+
+	void feature_callback(const std_msgs::Float64MultiArray::ConstPtr& msg) {
+		zeta << msg->data[0], msg->data[1], msg->data[2], msg->data[3], msg->data[4], msg->data[5], msg->data[6], msg->data[7];
+		if (getFeatures) {
+			for (size_t i=7; i>0; --i)
+				zeta_h.col(i) = zeta_h.col(i-1);
+			zeta_h.col(0) = zeta;
+		} else {
+			zeta_d = zeta;
+			for (size_t i=0; i<8; ++i)
+				zeta_h.col(i) = zeta;
+			getFeatures = !getFeatures;
+		}
+		zeta = 0.125*zeta_h.rowwise().sum();
+            	for (size_t i=0; i<4; ++i) {
+            		L.row(2*i+0) << -10.0,   0.0, 10.0*zeta(2*i+0), zeta(2*i+0)*zeta(2*i+1), -1.0-zeta(2*i+0)*zeta(2*i+0),  zeta(2*i+1);
+            		L.row(2*i+1) <<   0.0, -10.0, 10.0*zeta(2*i+1), 1.0+zeta(2*i+1)*zeta(2*i+1), -zeta(2*i+0)*zeta(2*i+1), -zeta(2*i+0);
+            	}
+	}
+	
+	void joint_callback (const sensor_msgs::JointState::ConstPtr& msg) {
+		q << msg->position[2], msg->position[1], msg->position[0], msg->position[3], msg->position[4], msg->position[5];
+		if (!getJoints) {
+			get_pose_jacobian();
+			x0 = x;
+			R0 = R;
+			getJoints = !getJoints;
+		}
+	}
+	
+	virtual void move_one_step () {
+		dq << upsilon, omega;
+		dq = J.transpose()*(J*J.transpose()).inverse()*dq;
+		for (size_t i=0; i<6; i++)
+			jv_msg.data[i] = dq(i);
+		pub_jv.publish(jv_msg);
+		ros::spinOnce();
+	}
+	
+	void stop_moving () {
+		upsilon.setZero();
+		omega.setZero();
+		move_one_step();
+	}
+	
+	void set_tar_pars () {
+		A_d.block(0*N,0*N,3*N,3*N) = kappa_u*kroneckerProduct(Snn,Matrix3f::Identity())+alpha_u*MatrixXf::Identity(3*N,3*N);
+		A_d.block(3*N,3*N,3*N,3*N) = alpha_o*MatrixXf::Identity(3*N,3*N);
+	}
+	
+	void update_tar_pars () {
+		b_d.head(3*N) = kappa_u*Sn*(x-xd);
+		b_d.tail(3*N) = kappa_o*Sn*skewVec(Rd.transpose()*R);
+	}
+	
+	void set_vis_pars () {
+		To << -Rec.transpose()*skewMat(xec), Rec.transpose();
+	}
+	
+	void update_vis_pars () {
+		Tu.block(0,0,3,3) = (R*Rec).transpose();
+		Lu = L*Tu;
+		Lo = L*To;
+		Lm << Lu, Lo;
+		A_v.block(0*N,0*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,Lu.transpose()*Lu);
+		A_v.block(0*N,3*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,Lu.transpose()*Lo);
+		A_v.block(3*N,0*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,Lo.transpose()*Lu);
+		A_v.block(3*N,3*N,3*N,3*N) = 5.0*gamma_v*kroneckerProduct(Snn,Lo.transpose()*Lo);
+		b_v.head(3*N) = gamma_v*Sn*Lu.transpose()*(zeta-zeta_d);
+		b_v.tail(3*N) = gamma_v*Sn*Lo.transpose()*(zeta-zeta_d);
+	}
+	
+	void set_cst_pars () {
+		Hu.block(0*N,0*N,3*N, 3*N) =  kroneckerProduct(Gamma,m*Matrix3f::Identity());
+		Ho.block(3*N,0*N,3*N, 3*N) = -kroneckerProduct(Gamma,I*Reo.transpose());
+		Hf.block(3*N,0*N,3*N,12*N) =  kroneckerProduct(MatrixXf::Identity(N,N),Go);
+	}
+	
+	void update_cst_pars () {
+		eta.head(3) = upsilon-R*xeo.cross(omega);
+		lambda.head(3) = I*Reo.transpose()*omega;
+		h.head(3*N) = m*eta-m*dt*kroneckerProduct(MatrixXf::Ones(N,1),R*skewMat(omega)*skewMat(omega)*xeo+g);
+		h.tail(3*N) = lambda-dt*kroneckerProduct(MatrixXf::Ones(N,1),skewMat(Reo.transpose()*omega)*I*Reo.transpose()*omega);
+		Ho.block(0*N,0*N,3*N, 3*N) = kroneckerProduct(Gamma,m*R*skewMat(xeo));
+		Hf.block(0*N,0*N,3*N,12*N) = kroneckerProduct(MatrixXf::Identity(N,N),R*Reo*Gu);
+	}
+
+	void store_data (float t_duration) {
+		if (getJoints && getFeatures) {
+			ofstream data_stream;
+			if (!file_init) {
+				data_stream.open(file_name.str());
+				file_init = !file_init;
+			} else
+				data_stream.open(file_name.str(),ios_base::app);
+			data_stream << setiosflags(ios::fixed) << setprecision(2) << ros::Time::now().toSec();
+			data_stream << ", " << setprecision(4) << t_duration;
+			vector<float*>::iterator it;
+			for (it=data.begin(); it!=data.end(); ++it)
+				data_stream << ", " << setprecision(3) << **it;
+			data_stream << endl;
+			data_stream.close();
+		}
+	}
+
+};
+
---
+++ experiments/controllers/src/mpc_right.cpp
@@ -0,0 +1,114 @@
+#include <gurobi_c++.h>
+#include "mpc_side.hpp"
+
+int main(int argc, char *argv[])
+{
+	init();
+	// set ROS
+	ros::init(argc,argv,"mpc_right");
+	// set control mode
+	float mode;
+	if (argc<2) {
+		cout << "Please set the control mode:" << endl;
+		cout << "0 for visual servoing." << endl;
+		cout << "1 for target reaching." << endl;
+		cout << "2 for combined control." << endl;
+		return 1;
+	} else {
+		mode = stod(argv[1]);
+		if (mode!=0.0 && mode!=1.0 && mode!=2.0) {
+			ROS_ERROR("Wrong mode.");
+			return 1;
+		}
+	}
+	// create the arm object
+	manipulator_side arm("arm_r","arm_l",mode);
+	// set the DH parameters of the arm
+	arm.a << 0.0, -0.24355, -0.21320, 0.0, 0.0, 0.0;
+	arm.alpha << M_PI/2.0, 0.0, 0.0, M_PI/2.0, -M_PI/2.0, 0.0;
+	arm.d << 0.15185, 0.0, 0.0, 0.13105, 0.08535, 0.0921;
+	// set the object pose relative to the end-effector frame of the arm
+	arm.xeo << 0.0, 0.20, -0.05;
+	arm.Reo << 1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, -1.0;
+	// set the pose of camera frame relative to tne end-effector frame of the arm
+	arm.xec << -0.035, -0.050, -0.075;
+	arm.Rec << 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0;
+	// set the pose of the arm base
+	arm.xb << 0.0, 0.44, 0.0;
+	arm.Rb << cos(M_PI/4.0), -sin(M_PI/4.0), 0.0, sin(M_PI/4.0), cos(M_PI/4.0), 0.0, 0.0, 0.0, 1.0;
+	// set the desired pose of the arm
+	arm.xd << 0.4008, 0.1000, 0.15;
+	arm.Rd << cos(-M_PI/4.0), sin(-M_PI/4.0), 0.0, sin(-M_PI/4.0), -cos(-M_PI/4.0), 0.0, 0.0, 0.0, -1.0;
+	// set parameters for Gurobi to solve the problem
+	arm.set_opt_pars();
+	// call Gurobi to perform optimal control
+	GRBEnv env = GRBEnv();
+	env.set(GRB_IntParam_OutputFlag, 0);
+	while (ros::ok()) {
+		// record the starting time of each round
+		auto t_start = chrono::high_resolution_clock::now();
+		// get the current robot pose and Jacobian
+		arm.get_pose_jacobian();
+		// update parameters for Gurobi to the solve the problem
+		arm.update_opt_pars();
+		if (arm.getJoints && arm.getFeatures && arm.getPeer) {
+			try {
+				// create a gurobi model and add optimization variables uof with lower and upper bounds
+				GRBModel model = GRBModel(env);
+				GRBVar *uof = model.addVars(arm.uof_lb,arm.uof_ub,NULL,NULL,NULL,18*N);
+				// set the objective function
+				GRBQuadExpr obj = 0.0;
+				for (size_t i=0; i<18*N; ++i) {
+					for (size_t j=0; j<18*N; ++j)
+						obj += dt*uof[i]*arm.A_obj(i,j)*uof[j];
+					obj += 2.0*arm.b_obj(i)*uof[i];
+				}
+				model.setObjective(obj);
+				// add motion constraints on the transported object
+				for (size_t i=0; i<6*N; ++i) {
+					GRBLinExpr cst = 0.0;
+					for (size_t j=0; j<3*N; ++j)
+						cst += arm.Hu(i,j-0*N)*uof[j];
+					for (size_t j=3*N; j<6*N; ++j)
+						cst -= arm.Ho(i,j-3*N)*uof[j];
+					for (size_t j=6*N; j<18*N; ++j) {
+						cst -= dt*arm.Hf(i,j-6*N)*uof[j];
+					}
+					model.addConstr(cst==arm.h(i));
+				}
+				// add constraints on the contact forces between the object and the tray
+				for (size_t n=0; n<N; ++n) {
+					for (size_t i=0; i<4; ++i) {
+						size_t ind = 6*N+12*n+3*i;
+						GRBQuadExpr cstc = uof[ind+0]*uof[ind+0]+uof[ind+1]*uof[ind+1]-mu*mu*uof[ind+2]*uof[ind+2];
+						model.addQConstr(cstc<=0.0);
+					}
+				}
+				// solve and set the Cartesian velocity control inputs
+				model.optimize();
+				for (size_t i=0; i<3; ++i) {
+					arm.upsilon(i) = uof[i].get(GRB_DoubleAttr_X);
+					arm.omega(i) = uof[3*N+i].get(GRB_DoubleAttr_X);
+				}
+				for (size_t i=0; i<12; ++i)
+					arm.f(i) = uof[6*N+i].get(GRB_DoubleAttr_X);
+			} catch(GRBException e) {
+				cout << "No solution. Reduce N or increase alpha." << endl;
+			} catch(...) {
+				cout << "Exception during optimization." << endl;
+			}
+		}
+		arm.move_one_step();
+		// count the time spent and store data
+		auto t_stop = chrono::high_resolution_clock::now();
+    		auto t_duration = chrono::duration<float>(t_stop-t_start);
+		arm.store_data(t_duration.count());
+    		// fix the time duration of each round
+    		if (t_duration.count()<dt)
+    			this_thread::sleep_for(chrono::duration<float>(dt-t_duration.count()));
+	}
+	arm.stop_moving();
+	
+	return 0;
+}
+
