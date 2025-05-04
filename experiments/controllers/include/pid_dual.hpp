e0e0f83f821bd11570731f2e12b505263d32693b
Yuan Yang <136157134+yuanyang0563@users.noreply.github.com>
2025-05-04 16:33:31 +0800

    Add files via upload

---
+++ experiments/controllers/include/pid_dual.hpp
@@ -0,0 +1,88 @@
+#include "manipulator.hpp"
+
+class manipulator_dual {
+
+  public:
+  
+  	manipulator left;
+  	manipulator right;
+
+  	float mode;
+  	
+  	bool getJoints, getFeatures;
+  	
+  	manipulator_dual (string arm_left, string arm_right, float control_mode) : left(arm_left), right(arm_right), mode(control_mode) {
+		getJoints = false;
+		getFeatures = false;
+		if (mode==0.0)
+			gamma_pid = 5.0;
+  	}
+  	
+  	void get_pose_jacobian () {
+  		left.get_pose_jacobian();
+  		right.get_pose_jacobian();
+  		if (!getJoints)
+  			getJoints = left.getJoints && right.getJoints;
+  	}
+  	
+  	void move_one_step () {
+  		left.move_one_step();
+  		right.move_one_step();
+  	}
+  	
+  	void stop_moving () {
+  		left.stop_moving();
+  		right.stop_moving();
+  	}
+  	
+  	void set_vis_pars () {
+		left.set_vis_pars();
+		right.set_vis_pars();
+	}
+	
+	void update_vis_pars () {
+		left.update_vis_pars();
+		right.update_vis_pars();
+		if (!getFeatures)
+			getFeatures = left.getFeatures && right.getFeatures;
+	}
+	
+	void get_vel_inputs () {
+		Vector3f upsilon_ld = kappa_pid*(left.xd-left.x);
+		Vector3f upsilon_rd = kappa_pid*(right.xd-right.x);
+		Vector3f omega_ld = kappa_pid*left.R.transpose()*skewVec(left.Rd*left.R.transpose());
+		Vector3f omega_rd = kappa_pid*right.R.transpose()*skewVec(right.Rd*right.R.transpose());
+		Vector3f upsilon_ls = rho_pid*(right.x-left.x-0.5*(left.R*left.R0.transpose()+right.R*right.R0.transpose())*(right.x0-left.x0));
+		Vector3f upsilon_rs = rho_pid*(left.x-right.x-0.5*(right.R*right.R0.transpose()+left.R*left.R0.transpose())*(left.x0-right.x0));
+		Vector3f omega_ls = rho_pid*left.R.transpose()*skewVec(right.R*right.R0.transpose()*left.R0*left.R.transpose());
+		Vector3f omega_rs = rho_pid*right.R.transpose()*skewVec(left.R*left.R0.transpose()*right.R0*right.R.transpose());
+		VectorXf twist_lv = gamma_pid*(left.Lm.transpose()*left.Lm).inverse()*left.Lm.transpose()*(left.zeta_d-left.zeta);
+		VectorXf twist_rv = gamma_pid*(right.Lm.transpose()*right.Lm).inverse()*right.Lm.transpose()*(right.zeta_d-right.zeta);
+		left.upsilon = upsilon_ls;
+		left.omega = omega_ls;
+		right.upsilon = upsilon_rs;
+		right.omega = omega_rs;
+		if (mode!=0) {
+			left.upsilon += upsilon_ld;
+			left.omega += omega_ld;
+			right.upsilon += upsilon_rd;
+			right.omega += omega_rd;
+		}
+		if (mode!=1) {
+			left.upsilon += twist_lv.head(3);
+			left.omega += twist_lv.tail(3);
+			right.upsilon += twist_rv.head(3);
+			right.omega += twist_rv.tail(3);
+		}
+		left.upsilon = left.upsilon.array().min(vt).max(-vt);
+		left.omega = left.omega.array().min(vr).max(-vr);
+		right.upsilon = right.upsilon.array().min(vt).max(-vt);
+		right.omega = right.omega.array().min(vr).max(-vr);
+	}
+  	
+  	void store_data (float t_duration) {
+		left.store_data(t_duration);
+		right.store_data(t_duration);
+	}
+
+};
---
+++ experiments/controllers/src/pid_left.cpp
@@ -0,0 +1,62 @@
+#include "pid_side.hpp"
+
+int main(int argc, char *argv[])
+{
+	init();
+	// set ROS
+	ros::init(argc,argv,"pid_left");
+	// set control mode
+	float mode;
+	if (argc<2) {
+		cout << "Please set the control mode:" << endl;
+		cout << "0 for visual servoing." << endl;
+		cout << "1 for target reaching." << endl;
+		cout << "2 for combined control." << endl;
+		return 1;
+	} else {
+		mode = stod(argv[1]);
+		if (mode!=0.0 && mode!=1.0 && mode!=2.0) {
+			ROS_ERROR("Wrong mode.");
+			return 1;
+		}
+	}
+	// create the arm object
+	manipulator_side arm("arm_l","arm_r",mode);
+	// set the DH parameters of the arm
+	arm.a << 0.0, -0.24365, -0.21325, 0.0, 0.0, 0.0;
+	arm.alpha << M_PI/2.0, 0.0, 0.0, M_PI/2.0, -M_PI/2.0, 0.0;
+	arm.d << 0.15190, 0.0, 0.0, 0.11235, 0.08535, 0.0819;
+	// set the pose of the camera frame relative to tne end-effector frame of the arm
+	arm.xec << 0.035, 0.050, -0.075;
+	arm.Rec << 0.0, -1.0, 0.0, 0.0, 0.0, -1.0, 1.0, 0.0, 0.0;
+	// set the pose of the arm base
+	arm.xb << 0.0, -0.44, 0.0;
+	arm.Rb << cos(3.0*M_PI/4.0), -sin(3.0*M_PI/4.0), 0.0, sin(3.0*M_PI/4.0), cos(3.0*M_PI/4.0), 0.0, 0.0, 0.0, 1.0;
+	// set the desired pose of the arm
+	arm.xd << 0.1392, -0.1616, 0.15;
+	arm.Rd << cos(-M_PI/4.0), sin(-M_PI/4.0), 0.0, sin(-M_PI/4.0), -cos(-M_PI/4.0), 0.0, 0.0, 0.0, -1.0;
+	// set parameters for visual servoing
+	arm.set_vis_pars();
+	while (ros::ok()) {
+		// record the starting time of each round
+		auto t_start = chrono::high_resolution_clock::now();
+		// get the current robot pose and Jacobian
+		arm.get_pose_jacobian();
+		// update parameters for visual servoing
+		arm.update_vis_pars();
+		if (arm.getJoints && arm.getFeatures && arm.getPeer)
+			arm.get_vel_input();
+		arm.move_one_step();
+		// count the time spent and store data
+		auto t_stop = chrono::high_resolution_clock::now();
+    		auto t_duration = chrono::duration<float>(t_stop-t_start);
+    		arm.store_data(t_duration.count());
+    		// fix the time duration of each round
+    		if (t_duration.count()<dt)
+    			this_thread::sleep_for(chrono::duration<float>(dt-t_duration.count()));
+	}
+	arm.stop_moving();
+	
+	return 0;
+}
+
